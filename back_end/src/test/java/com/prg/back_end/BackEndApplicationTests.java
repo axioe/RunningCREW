d3fa270e7b7e95741701cdfc55eb68e817c4e3b8
axioe <yhkimb@naver.com>
2026-06-10 09:18:12 +0900

    feat: 프론트엔드 메인화면 구현 및 백엔드 구조 구축

---
+++ back_end/src/test/java/com/prg/back_end/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prg.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ front_end/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// index.css가 css 폴더로 이동했으므로 경로 수정! (안 쓰신다면 주석 처리해도 무방합니다)
+import './css/index.css' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
