805df5be94b99e738018d777ceb379d4083a4832
sos-cyber <shinohshim@gmail.com>
2026-07-01 13:24:42 +0900

    feat : AWS 배포 수정 작업

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -9,11 +9,11 @@ import Main from "./components/Main.jsx";
 import Course from "./components/Course.jsx";
 import Search from "./components/Search.jsx";
 import Crew from "./components/Crew.jsx";
-import Login from "./components/login/Login.jsx";
-import SignUp from "./components/login/SignUp.jsx";
+import Login from "./components/Login/Login.jsx";
+import SignUp from "./components/Login/SignUp.jsx";
 import MyPage from "./components/MyPage.jsx";
 import Post from "./components/Post.jsx";
-import LoginSearch from "./components/login/LoginSearch.jsx";
+import LoginSearch from "./components/Login/LoginSearch.jsx";
 import CourseDetail from "./components/CourseDetail.jsx";
 import AdminPage from "./components/admin/AdminPage.jsx";
 import useAuthStore from "./components/common/useAuthStore.jsx";
@@ -28,40 +28,29 @@ function App() {
     <Router>
       <Routes>
         {/* 홈 (메인페이지) */}
-        <Route path="/" element={<Main />} />
-
+        <Route path="/" element={<Main />} />s
         {/* 메뉴의 '러닝 코스' 전용 경로 */}
         <Route path="/course" element={<Course />} />
-
         {/* 크루 모집 페이지 */}
         <Route path="/crew" element={<Crew />} />
-
         {/* 메인 검색 버튼 전용 독립 경로 */}
         <Route path="/search" element={<Search />} />
-
         {/* 마이페이지 버튼 전용 독립 경로 */}
         <Route path="/mypage" element={<MyPage />} />
         {/* 게시글 상세 페이지 (신청자 관리 포함) */}
         <Route path="/post/:id" element={<MyPostDetail />} />
-
         {/* 러닝 코스 상세 페이지 */}
         <Route path="/course-detail" element={<CourseDetail />} />
-
         {/* 글쓰기 페이지 */}
         <Route path="/write" element={<Post />} />
-
         {/* 로그인 버튼 전용 독립 경로 */}
         <Route path="/login" element={<Login />} />
-
         {/* 비밀번호 찾기 페이지 */}
         <Route path="/LoginSearch" element={<LoginSearch />} />
-
         {/* 회원가입 전용 경로 */}
         <Route path="/signup" element={<SignUp />} />
-
         {/* 재난안전 전용 경로 */}
         <Route path="/safety" element={<PublicSafety />} />
-
         {/* 관리자 페이지 전용 독립 경로 */}
         <Route
           path="/adminMain"
